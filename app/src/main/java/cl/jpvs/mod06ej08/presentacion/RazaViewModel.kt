20e39e782ee44c8ca2ba2d1fbf2493c706bf3196
JpVargass <136398972+JpVargass@users.noreply.github.com>
2023-08-18 04:30:17 -0400

    funcionalidad presentacion ViewModel

--- app/src/main/java/cl/jpvs/mod06ej08/presentacion/RazaViewModel.kt
+++ app/src/main/java/cl/jpvs/mod06ej08/presentacion/RazaViewModel.kt
@@ -1,4 +1,22 @@
 package cl.jpvs.mod06ej08.presentacion
 
-class RazaViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import cl.jpvs.mod06ej08.data.Repositorio
+import cl.jpvs.mod06ej08.data.local.RazaDatabase
+import cl.jpvs.mod06ej08.data.remote.RazaRetrofit
+import kotlinx.coroutines.launch
+
+class RazaViewModel (application: Application): AndroidViewModel(application) {
+    private val repositorio: Repositorio
+
+    init {
+        val api = RazaRetrofit.getRetrofitRaza()
+        val razaDatabase = RazaDatabase.getDatabase(application).getRazaDao()
+        repositorio = Repositorio(api,razaDatabase)
+    }
+    fun getAllRazas() = viewModelScope.launch {
+        repositorio.getRazas()
+    }
 }
